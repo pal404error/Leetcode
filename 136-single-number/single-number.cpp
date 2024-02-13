236b3c8e1efbe97277cc777ead15bc461850f0d0
Pal <82200759+pal404error@users.noreply.github.com>
2024-02-13 19:28:20 +0530

    Time: 20 ms (33.66%) | Memory: 23.7 MB (5.63%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> map;
+        for(auto i :nums){
+            map[i]++;
+        }
+        int ans;
+        for(auto n :map){
+            if(n.second == 1){
+                ans = n.first;
+            }
+        }
+        return ans;
+    }
+};
