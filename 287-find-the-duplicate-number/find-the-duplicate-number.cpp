8e89ce1810d3b2044785a62dd7919e78561f1a40
Pal <82200759+pal404error@users.noreply.github.com>
2024-03-24 18:09:16 +0530

    Time: 101 ms (39.94%) | Memory: 64.8 MB (26.28%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -4,7 +4,10 @@ public:
         int n=0,ans=0;
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-1;i++){
-            n =( nums[i] ^ nums[i+1])? -1 : ans = nums[i];
+
+            n =nums[i] ^ nums[i+1] ;
+
+            if(n == 0) {ans = nums[i];break;}
         } 
         
         return ans;
