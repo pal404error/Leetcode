393b8a6680d631b30c3bcecc346ec93283e6820f
Pal <82200759+pal404error@users.noreply.github.com>
2024-03-26 12:59:30 +0530

    Time: 18 ms (38.00%) | Memory: 19.1 MB (86.29%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if(nums.size() ==1){
-            return nums[0];
-        }
+        
         int x=0;
         for(auto i:nums){
             x ^= i;
