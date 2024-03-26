fdd10557d8ecbba84a20479892508f0f7bf84659
Pal <82200759+pal404error@users.noreply.github.com>
2024-03-26 10:55:36 +0530

    Time: 66 ms (19.06%) | Memory: 44.6 MB (30.70%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int m=0;
-        if(nums.size() == 1 && nums[0] == 1){
-
-        }
         for(auto i:nums) m = max(m,i);
         sort(nums.begin(), nums.end());
         for(int i=1;i<m;i++){
-            if(binary_search(nums.begin(), nums.end(), i)){
-
-            }else return i;
+            if(!binary_search(nums.begin(), nums.end(), i)){
+                return i;
+            } 
         
         }
         return m+1;
