8f916f73e41c2389655d7de227a70b0cedaa9786
Pal <82200759+pal404error@users.noreply.github.com>
2024-04-08 19:20:28 +0530

    Time: 35 ms (84.79%) | Memory: 16.5 MB (59.90%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        stu = students
+        sand = sandwiches
+        c1 = students.count(1)
+        c0 = students.count(0)
+        print(c1)
+        for s in sand:
+            if(s == 0 and c0 == 0):
+                return c1
+            if( s == 1 and c1 == 0):
+                return c0
+            if( s == 0 ):
+                c0 -= 1
+            else:
+                c1-=1
+            
+        
+        return 0
