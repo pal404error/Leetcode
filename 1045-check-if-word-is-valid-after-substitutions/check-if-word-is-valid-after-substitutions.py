77cd598c87ebda290422bf2c8d13d7b4b63fc2c0
Pal <82200759+pal404error@users.noreply.github.com>
2024-05-25 19:52:08 -0400

    Time: 42 ms (80.35%) | Memory: 16.5 MB (98.92%) - LeetSync

--- 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.py
+++ 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.py
@@ -1,11 +1,10 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         temp = s
-        for i in s:
-            if len(temp) == 0:
-                return True
-            q = temp.replace("abc", '')
-            temp = q
-        return False
+        while temp:
+            if not "abc" in temp:
+                return False
+            temp = temp.replace("abc", '')
+        return True
 
                 
