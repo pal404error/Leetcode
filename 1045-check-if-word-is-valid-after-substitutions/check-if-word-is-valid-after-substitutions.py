cdb2e2185204e8ba0538102b1720c3b295e6504a
Pal <82200759+pal404error@users.noreply.github.com>
2024-05-25 19:49:06 -0400

    Time: 1615 ms (5.18%) | Memory: 16.8 MB (53.13%) - LeetSync

--- 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.py
+++ 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.py
@@ -1,15 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        z = []
         temp = s
         for i in s:
             if len(temp) == 0:
                 return True
             q = temp.replace("abc", '')
             temp = q
-        if len(temp) != 0:
-            return False
-
-        return True
+        return False
 
                 
