e6770379c413e22d7e5c93a86ce390cc518d0aeb
Pal <82200759+pal404error@users.noreply.github.com>
2024-05-29 09:41:48 -0400

    Time: 31 ms (85.86%) | Memory: 16.6 MB (62.56%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -3,7 +3,7 @@ class Solution:
         a = int(s, 2)
         n=0
         while True:
-            print(a)
+            # print(a)
             if a == 1:
                 break
             if a%2 == 0:
