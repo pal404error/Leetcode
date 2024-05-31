8126741753eefb8e652c5d9fd9607e5d0282f759
Pal <82200759+pal404error@users.noreply.github.com>
2024-05-31 11:13:45 -0400

    Time: 458 ms (15.87%) | Memory: 18.2 MB (96.72%) - LeetSync

--- 260-single-number-iii/single-number-iii.py
+++ 260-single-number-iii/single-number-iii.py
@@ -2,9 +2,10 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
         ans = []
         for i in nums:
-            if i not in ans:
-                ans.append(i)
-            else:
+            if i in ans:
                 ans.remove(i)
+                
+            else:
+                ans.append(i)
         
         return ans
