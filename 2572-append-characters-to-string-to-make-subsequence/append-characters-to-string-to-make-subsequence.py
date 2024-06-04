b9010d3caed2a57782a736955ef3d9bd27f0fab8
Pal <82200759+pal404error@users.noreply.github.com>
2024-06-03 20:09:14 -0400

    Time: 75 ms (18.05%) | Memory: 17.5 MB (96.10%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.py
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.py
@@ -4,6 +4,8 @@ class Solution:
         while l< len(s) and r < len(t):
             if s[l] == t[r]:
                 r+=1
+                if r == len(t):
+                    return 0
             l+=1
                 
         return len(t) - r
