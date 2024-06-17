c924a0ff5a02e46aeeeff178799ff806a869772c
Pal <82200759+pal404error@users.noreply.github.com>
2024-06-17 09:43:32 -0400

    Time: 96 ms (53.57%) | Memory: 16.4 MB (79.87%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        s,l = 0, int(math.sqrt(c))
+        print(l)
+        while s<=l:
+            sum = (s*s) + (l*l)
+            if sum == c:
+                return True
+            elif sum < c:
+                s+=1
+            elif sum>c:
+                l-=1
+        
+        
+        return False
