2a49dc8f0aeb83fdd8e40d45a9af170d74d96a75
Pal <82200759+pal404error@users.noreply.github.com>
2026-03-07 23:03:44 +0530

    Time: 3 ms (34.69%) | Memory: 19.3 MB (63.99%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -2,17 +2,16 @@ class Solution:
     def isValid(self, s: str) -> bool:
         a = []
         for i in s:
-            if i == "{" or i == "(" or i == "[":
+            if i == "{" or i=="[" or i=="(":
                 a.append(i)
-            elif len(a) != 0 and a[-1] == "{" and i == "}":
+            elif len(a) != 0 and a[-1] == "{" and i=="}":
                 a.pop()
-            elif len(a) != 0 and a[-1] == "(" and i == ")":
+            elif len(a) != 0 and a[-1] == "(" and i==")":
                 a.pop()
-            elif len(a) != 0 and a[-1] == "[" and i == "]":
+            elif len(a) != 0 and a[-1] == "[" and i=="]":
                 a.pop()
             
-                
             else:
                 return False
         
-        return len(a) == 0
+        return len(a) ==0
