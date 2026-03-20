5172204769670c54ae2fe1fb8f0f76c314d8b095
Pal <82200759+pal404error@users.noreply.github.com>
2026-03-21 02:30:59 +0530

    Time: 38 ms (89.25%) | Memory: 19.4 MB (20.38%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.py
@@ -0,0 +1,15 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        i=1
+        j=n
+        while(i<j):
+            p = (i+j) // 2
+            if( isBadVersion(p)):
+                j=p
+            else:
+                i=p+1
+        return i
+        
